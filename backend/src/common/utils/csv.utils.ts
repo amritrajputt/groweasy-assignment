bf493da729f908d4913caa750d67464985b7da3b
amrit <vishal179singh@gmail.com>
2026-07-08 18:38:16 +0530

    csv parsing with batching

---
+++ backend/src/common/utils/csv.utils.ts
@@ -0,0 +1,44 @@
+import papaParse from "papaparse";
+import { ReadStream } from "fs";
+
+export const parseCSVStreamInBatches = (
+    stream: ReadStream,
+    batchSize: number,
+    onBatch: (chunkOfRows: any[]) => Promise<void>
+) => {
+    return new Promise<void>((resolve, reject) => {
+        let rowBuffer: any[] = [];
+
+        papaParse.parse(stream, {
+            header: true,
+            dynamicTyping: true, //auto convert to types
+            skipEmptyLines: true,
+            step: async (results, parser) => {
+                rowBuffer.push(results.data);
+
+                if (rowBuffer.length >= batchSize) {
+                    parser.pause();
+                    try {
+                        await onBatch(rowBuffer);
+                        rowBuffer = [];
+                        parser.resume();
+                    } catch (error) {
+                        parser.abort();
+                        reject(error);
+                    }
+                }
+            },
+            complete: async () => {
+                try {
+                    if (rowBuffer.length > 0) {
+                        await onBatch(rowBuffer);
+                    }
+                    resolve();
+                } catch (error) {
+                    reject(error);
+                }
+            },
+            error: reject,
+        });
+    });
+};
