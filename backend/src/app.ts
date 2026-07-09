1afcdb5df256e63dce2e5da44509c82b2fb0dce1
amrit <vishal179singh@gmail.com>
2026-07-09 22:24:02 +0530

    backend completed

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,18 +1,23 @@
-import express, { urlencoded } from "express";
 import dotenv from "dotenv";
+dotenv.config();
+
+import express, { urlencoded } from "express";
 import cors from "cors";
+import { serve } from "inngest/express";
 import { errorHandler } from "./common/middleware/error.middleware"; 
 import importRouter from "./modules/import/import.routes";
-dotenv.config();
+import { inngest } from "./inngest/inngest-client";
+import { functions } from "./inngest/inngest-function.js";
 
 const app = express();
 
 
 app.use(cors({ origin: "http://localhost:3000" }));
-app.use(urlencoded({ extended: true }));
-app.use(express.json());
+app.use(urlencoded({ limit: "5mb", extended: true }));
+app.use(express.json({ limit: "5mb" }));
 
 app.use("/api/import", importRouter);
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 
 app.get("/health", (req, res) => {
